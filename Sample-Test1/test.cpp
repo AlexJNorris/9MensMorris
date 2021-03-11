c3311fca9105ff7fe9bd1aca837e5748379ef856
Ian Chacey <basebal360@gmail.com>
2021-03-11 16:34:36 -0600

    added working tests

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "../9MensMorris/Board.h"
+#include "Board.h"
 
 TEST(TestCaseName, TestName) {
 	  morisGame* Board = new morisGame;
@@ -7,4 +8,19 @@ TEST(TestCaseName, TestName) {
 	  Board->setBoardPiece(0);
 	  bool chk = Board->boardSpaces[0]->isPlayerOne();
 	  ASSERT_EQ(chk, true);
+}
+
+TEST(TestCaseName1, TestName1) {
+	morisGame* Board = new morisGame;
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	bool chk = Board->boardSpaces[0]->isPlayerOne();
+	ASSERT_EQ(chk, false);
+}
+
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
