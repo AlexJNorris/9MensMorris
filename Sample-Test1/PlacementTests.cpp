15486766edd969ae318ab9c4104e4cc714a5d5ef
AlexJNorris <a.norris97@icloud.com>
2021-05-06 19:15:37 -0500

    Apply Unit Testing

--- Sample-Test1/PlacementTests.cpp
+++ Sample-Test1/PlacementTests.cpp
@@ -1,8 +1,8 @@
 ﻿#include "pch.h"
 #include "../9MensMorris/Board.h"
+#include "../9MensMorris/Computer.h"
 
-
-//AC 1.1 Test will initilzie game and it will be player 1's turn
+//AC 1.1 Test will initialize game and it will be player 1's turn
 //After player 1 is done will then initialize player 2
 TEST(PlayerTurn, Player1) {
 	morrisGame* Board = new morrisGame;
@@ -15,7 +15,7 @@ TEST(PlayerTurn, Player1) {
 	ASSERT_EQ(playerNum, 1);
 }
 
-//AC 1.2 Test will initilzie game 
+//AC 1.2 Test will initialize game 
 //After player 1 makes a move it will be player 2's turn
 TEST(PlayerTurn, Player2) {
 	morrisGame* Board = new morrisGame;
@@ -26,7 +26,7 @@ TEST(PlayerTurn, Player2) {
 	ASSERT_EQ(playerNum, 1);
 }
 
-//AC 1.3 Test will incorrectly initilzie game
+//AC 1.3 Test will incorrectly initialize game
 //It will be player 2's turn without player 1 making a valid move
 TEST(PlayerTurn, SkipPlayer1) {
 	morrisGame* Board = new morrisGame;
@@ -36,7 +36,7 @@ TEST(PlayerTurn, SkipPlayer1) {
 	ASSERT_NE(playerNum, 0);
 }
 
-//AC 1.4 Test will incorrectly initilzie game
+//AC 1.4 Test will incorrectly initialize game
 //When it should be player 2's turn, the game will still recognize it as player 1's turn
 TEST(PlayerTurn, SkipPlayer2) {
 	morrisGame* Board = new morrisGame;
@@ -63,11 +63,9 @@ TEST(GameboardCreation, Unsuccessful) {
 	ASSERT_NE(playerNum, 0);
 }
 
-
 //AC 3.1 Test placing a red piece at location 0
 //After succesful placement should be Player 2's turn
 TEST(TestPlacement, RedPlacement) {
-
 	morrisGame* Board = new morrisGame;
 	Board->setBoard();
 	Board->setBoardPiece(0);
@@ -94,7 +92,7 @@ TEST(TestPlacement, BlackPlacement) {
 }
 
 //AC 3.3 Test placing a red piece on location out of range
-//Test should cause an exception to be trown since placement is out of range
+//Test should cause an exception to be thrown since placement is out of range
 TEST(TestPlacement, InvalidRedPlacement) {
 	morrisGame* Board = new morrisGame;
 	Board->setBoard();
@@ -120,13 +118,11 @@ TEST(TestPlacement, InvalidRedPlacementOnBlack) {
 	else {
 		chk = true;
 	}
-
 	EXPECT_EQ(chk, true);
 	chk = Board->boardSpaces[0]->isPlayerTwo();
 	EXPECT_EQ(chk, true);
 	int playerNum = (Board->getTurn() % 2);
 	ASSERT_NE(playerNum, 1);
-
 }
 
 //AC 3.5 Test placing a black piece on location out of range
@@ -156,13 +152,11 @@ TEST(TestPlacement, InvalidBlackPlacementOnRed) {
 	else {
 		chk = true;
 	}
-
 	EXPECT_EQ(chk, true);
 	chk = Board->boardSpaces[0]->isPlayerOne();
 	EXPECT_EQ(chk, true);
 	int playerNum = (Board->getTurn() % 2);
 	ASSERT_NE(playerNum, 0);
-
 }
 
 //AC 4.1 Successfully move a red piece to a valid location
@@ -211,14 +205,13 @@ TEST(PieceMovement, InvalidRedMovement) {
 	Board->setBoard();
 	Board->setBoardPiece(0);
 	bool chk = Board->boardSpaces[0]->isPlayerOne();
-	//(chk, true);
+	EXPECT_EQ(chk, true);
 	Board->selected = 0;
-
 	EXPECT_ANY_THROW(Board->moveSelectedToPos(-1));
 }
 
 //AC 4.5 Unsuccessfully move a red piece to a location occupied by a black piece
-//Piece should not move from the original position and blacks piece will not change
+//Piece should not move from the original position and black’s piece will not change
 TEST(PieceMovement, InvalidRedMovementOntoBlack) {
 	morrisGame* Board = new morrisGame;
 	Board->setBoard();
@@ -260,7 +253,7 @@ TEST(PieceMovement, InvalidRedMovement2) {
 }
 
 //AC 4.8 Unsuccessfully move a Black piece to a location occupied by a Red piece
-//Piece should not move from the original position and reds piece will not change
+//Piece should not move from the original position and red’s piece will not change
 TEST(PieceMovement, InvalidBlackMovementOntoRed) {
 	morrisGame* Board = new morrisGame;
 	Board->setBoard();
@@ -279,6 +272,7 @@ TEST(PieceMovement, InvalidBlackMovementOntoRed) {
 	chk = Board->boardSpaces[1]->isPlayerOne();
 	ASSERT_EQ(chk, true);
 }
+
 //AC 5.1 Fly red across board
 TEST(Flying, RedFlying) {
 	morrisGame* Board = new morrisGame;
@@ -291,6 +285,7 @@ TEST(Flying, RedFlying) {
 	chk = Board->boardSpaces[23]->isPlayerOne();
 	ASSERT_EQ(chk, true);
 }
+
 //AC 5.2 Fly black across board
 TEST(Flying, BlackFlying) {
 	morrisGame* Board = new morrisGame;
@@ -303,6 +298,7 @@ TEST(Flying, BlackFlying) {
 	chk = Board->boardSpaces[23]->isPlayerTwo();
 	ASSERT_EQ(chk, true);
 }
+
 //AC 5.3 Fail to choose red to fly across board
 TEST(Flying, RedFailChoose) {
 	morrisGame* Board = new morrisGame;
@@ -317,13 +313,13 @@ TEST(Flying, RedFailChoose) {
 	if (Board->boardSpaces[22]->isEmpty())
 	{
 		Board->moveSelectedToPos(1);
-
 	}
 	chk = Board->boardSpaces[22]->isEmpty();
 	ASSERT_EQ(chk, true);
 	chk = Board->boardSpaces[1]->isEmpty();
 	ASSERT_EQ(chk, true);
 }
+
 //AC 5.4 & 5.5 Fail to Fly red across board
 TEST(Flying, RedFailFlying) {
 	morrisGame* Board = new morrisGame;
@@ -338,7 +334,6 @@ TEST(Flying, RedFailFlying) {
 	if (Board->boardSpaces[23]->isEmpty())
 	{
 		Board->moveSelectedToPos(23);
-
 	}
 	chk = Board->boardSpaces[23]->isPlayerTwo();
 	ASSERT_EQ(chk, true);
@@ -358,7 +353,6 @@ TEST(Flying, BlackFailChoose) {
 	if (Board->boardSpaces[22]->isEmpty())
 	{
 		Board->moveSelectedToPos(1);
-
 	}
 	chk = Board->boardSpaces[22]->isEmpty();
 	ASSERT_EQ(chk, true);
@@ -380,7 +374,6 @@ TEST(Flying, BlackFailFlying) {
 	if (Board->boardSpaces[0]->isEmpty())
 	{
 		Board->moveSelectedToPos(0);
-
 	}
 	chk = Board->boardSpaces[0]->isPlayerOne();
 	ASSERT_EQ(chk, true);
@@ -418,7 +411,9 @@ TEST(Removal, InvalidBlackRemoval) {
 	Board->setBoardPieceP2(0);
 	bool chk = Board->boardSpaces[0]->isPlayerTwo();
 	EXPECT_EQ(chk, true);
-	ASSERT_ANY_THROW(Board->removePiece(1););
+	if (Board->boardSpaces[1]->isPlayerTwo()) {
+		Board->removePiece(1);
+	}
 	chk = Board->boardSpaces[1]->isPlayerTwo();
 	ASSERT_NE(chk, true);
 }
@@ -449,6 +444,7 @@ TEST(Removal, InvalidRedRemoval) {
 	chk = Board->boardSpaces[1]->isPlayerOne();
 	ASSERT_NE(chk, true);
 }
+
 
 //AC 6.6 Unsuccessfully removal of a black piece when an invalid position is chosen for removal
 TEST(Removal, InvalidBlackRemovalByBlack) {
@@ -495,7 +491,6 @@ TEST(Distinguish, DistinguishBlack) {
 	ASSERT_EQ(chk, false);
 }*/
 
-
 TEST(TestBlackPlacement, TestName) {
 	morrisGame* Board = new morrisGame;
 	Board->setBoard();
@@ -513,7 +508,7 @@ TEST(TestBlackPlacement, TestName) {
 	ASSERT_EQ(chk, false);
 }*/
 
-//Test new mill placement for player 1
+//Test new mill placement for both players
 TEST(TestMill, TestMillOperations) {
 	morrisGame* Board = new morrisGame;
 	Board->setBoard();
@@ -538,6 +533,314 @@ TEST(TestMill, TestMillOperations) {
 	ASSERT_EQ(Board->p1MillArr[1][0], -1);
 	ASSERT_NE(Board->p2MillArr[0][0], -1);
 	ASSERT_EQ(Board->p2MillArr[1][0], -1);
+}
+
+//AC 8.1 Test computer placing a black piece after a valid Player move
+//Will then be player's turn.
+TEST(ComputerPlacement, ValidPlacement) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(1);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	Board->turns++;
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 0);
+}
+
+//AC 8.2 Test computer placing an invalid black piece after a valid Player move
+//Will stay computer's turn.
+TEST(ComputerPlacement, InvalidPlacement) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(1);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = -1;
+	EXPECT_ANY_THROW(Board->setBoardPieceP2(pos_););
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+
+//AC 8.3 Test computer placing an invalid black piece on a player's ocupided piece after a valid Player move
+//Will stay computer's turn.
+TEST(ComputerPlacement, InvalidPlacementOnPlayer) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(1);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = 1;
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[0]->isPlayerTwo();
+	EXPECT_NE(chk, true);
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+//AC 9.1 Test computer placing an invalid black piece on a player's ocupided piece after a valid Player move
+TEST(ComputerMovement, ValidMovement) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	Board->turns++;
+	//check if it's comp's turn
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	Board->setBoardPieceP2(1);
+	//check if comp placed a piece on space 1;
+	bool chk = Board->boardSpaces[1]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	//generates a point to move to.
+	int pos_ = cmp->makeMove(Board);
+	Board->selected = 1;
+	Board->moveSelectedToPos(pos_);
+	//check if comp placed a piece on space 1;
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	ASSERT_EQ(chk, true);
+}
+
+//AC 9.2 Computer unsuccessfully chooses a piece to move
+TEST(ComputerMovement, InvalidPieceChosen) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	Board->setBoardPieceP2(1);
+	bool chk = Board->boardSpaces[2]->isPlayerTwo();
+	ASSERT_NE(chk, true);
+}
+
+//AC 9.3 Computer unsuccessfully moves a piece to a location occupied by a player's piece
+//Piece should not move from the original position and player's piece will not change
+TEST(ComputerMovement, InvalidCompMovementOntoPlayer) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	//create board and place a piece for player
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	//computer generates a spot to place piece at
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	//check if the space is occupided by computer
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	//check if space 0 is occupied by player
+	chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_EQ(chk, true);
+	//select computers piece to move
+	Board->selected = pos_;
+	if (Board->boardSpaces[0]->isEmpty()) {
+		Board->moveSelectedToPos(0);
+	}
+	//will check if player's piece was removed
+	chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_EQ(chk, true);
+	//will check if computers piece was moved
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	ASSERT_EQ(chk, true);
+}
+
+//AC 9.4 Computer unsuccessfully move a piece to an invalid location
+//Test should cause an exception to be thrown since location to move to is out of range
+TEST(ComputerMovement, InvalidMovement) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	Board->selected = pos_;
+	EXPECT_ANY_THROW(Board->moveSelectedToPos(-1));
+}
+
+//AC 10.1 Fly Computers piece across board
+TEST(ComputerFlying, ValidFly) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	Board->selected = pos_;
+	Board->moveSelectedToPos(23);
+	chk = Board->boardSpaces[23]->isPlayerTwo();
+	ASSERT_EQ(chk, true);
+}
+
+//AC 10.2 Computer unsuccessfully chooses a piece to move
+//Then no piece will move and the computer's turn will continue
+TEST(ComputerFlying, InvalidChoice) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->turns++;
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	//invalid choice
+	Board->selected = pos_ + 1;
+	if (Board->boardSpaces[pos_ + 1]->isEmpty())
+	{
+		Board->moveSelectedToPos(1);
+	}
+	chk = Board->boardSpaces[pos_ + 1]->isEmpty();
+	EXPECT_EQ(chk, true);
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	int playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+
+//AC 10.3 Computer unsuccessfully flies to an invalid place
+TEST(ComputerFlying, InvalidFly) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->turns++;
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	Board->selected = pos_;
+	ASSERT_ANY_THROW(Board->moveSelectedToPos(-1););
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	int playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+
+//AC 10.4 Computer unsuccessfully flies to a player occupied space
+TEST(ComputerFlying, InvalidFlyOntoPlayer) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	Board->selected = pos_;
+	if (Board->boardSpaces[0]->isEmpty())
+	{
+		Board->moveSelectedToPos(0);
+	}
+	chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_EQ(chk, true);
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+
+//AC 11.1 Computer successfully removes a player piece
+TEST(ComputerRemoval, SuccessfulRemoval) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	bool chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_EQ(chk, true);
+	Board->removePiece(0);
+	chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_NE(chk, true);
+	Board->turns++;
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 0);
+}
+
+//AC 11.2 Computer unsuccessfully chooses invalid player piece to remove
+//Piece chosen will not be removed, and it will remain computers turn
+TEST(ComputerRemoval, InvalidRemoval) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(0);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	bool chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_EQ(chk, true);
+	Board->removePiece(1);
+	chk = Board->boardSpaces[0]->isPlayerOne();
+	EXPECT_EQ(chk, true);
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+
+//AC 11.3 Computer unsuccessfully chooses own piece to remove
+//Piece chosen will not be removed, and it will remain computers turn
+TEST(ComputerRemoval, InvalidRemovalOfOwn) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	if (!Board->boardSpaces[pos_]->isPlayerTwo())
+	{
+		Board->removePiece(pos_);
+	}
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	playerNum = (Board->getTurn() % 2);
+	ASSERT_EQ(playerNum, 1);
+}
+
+//AC Test computer placing a black piece after a valid Player move
+//Will then be player's turn.
+//Needs to be finished
+TEST(ComputerMillPlacement, ValidMillPlacement) {
+	morrisGame* Board = new morrisGame;
+	Computer* cmp = new Computer();
+	Board->setBoard();
+	Board->setBoardPiece(1);
+	Board->turns++;
+	int playerNum = (Board->getTurn() % 2);
+	EXPECT_EQ(playerNum, 1);
+	int pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	bool chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	pos_ = cmp->makeMove(Board);
+	Board->setBoardPieceP2(pos_);
+	chk = Board->boardSpaces[pos_]->isPlayerTwo();
+	EXPECT_EQ(chk, true);
+	chk = Board->isNewMillMade(1);
+	ASSERT_NE(chk, true);
 }
 
 int main(int argc, char** argv)
